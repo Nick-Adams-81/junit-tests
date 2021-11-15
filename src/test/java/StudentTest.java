2c6c424db570b7ad929ce4be2edbfb4cab86f0f7
nick adams <thebigdeal916@icloud.com>
2021-11-15 14:56:26 -0600

    test re factor

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,14 +1,12 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertNotNull;
 
 public class StudentTest {
 
-
     Student test = new Student("test", 2);
-
-
 
     @Test
     public void testCreateStudent() {
@@ -16,14 +14,19 @@ public class StudentTest {
     }
 
     @Test
-    public void testSetId() {
-        assertNotNull(test.getId());
+    public void testId() {
+        long expected = 2;
+        long actual = test.getId();
+        assertEquals(expected, actual);
     }
 
     @Test
     public void testName() {
-        assertNotNull(test.getName());
+        String expected = "test";
+        String actual = test.getName();
+        assertEquals(expected, actual);
     }
+
 
 
 }
